eacef32c4ac689c9aafcb38a8459fb57ac1dc7c2
Andres <androiz10@gmail.com>
2015-12-06 17:20:32 +0100

    Error PythonAnyWhere(PAW)

--- cine/cineREST/viewsets.py
+++ cine/cineREST/viewsets.py
@@ -1,8 +1,11 @@
-from serializers import *
+from cine.cineREST.serializers import FilmSerializer
+from cine.cineREST.models import Film
+
 from rest_framework import viewsets
 
-
 # ViewSets define the view behavior.
+
+
 class FilmViewSet(viewsets.ModelViewSet):
     queryset = Film.objects.all()
     serializer_class = FilmSerializer
